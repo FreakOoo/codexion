30fb5575a76f79bf4aab7f51d9b8b3a82a728910
Maxime Chopin <mchopin@f0r3s3.codam.nl>
2026-09-11 16:45:30 +0200

    small update

--- monitor.c
+++ monitor.c
@@ -1,8 +1,9 @@
 #include "codexion.h"
 
 //maybe it takes in coders as arguments to check their times
+//feeding in the array of t_persons
 
-void* monitor(void * arg)
+void* monitor(char * coders)
 {
   int live;
   live = 1;
@@ -10,6 +11,16 @@ void* monitor(void * arg)
 
   while(live)
   {
+    int i;
+    i = 0;
+    while(coders[i])
+    {
+      //lock
+      //read last compile
+      //use that to see if we hit deadline
+      //
+      //if past deadline, print death message
+    }
     //stuff here 
     sleep(1);
   }
